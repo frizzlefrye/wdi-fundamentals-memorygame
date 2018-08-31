a5d16db249ac3154b11af54a1543ee487d33c16c
Gregory Frye <Swayze@Gregorys-MacBook-Pro.local>
2018-08-31 12:46:50 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,26 @@
 var cards = ["queen", "queen", "king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+	}
+}
 
-console.log("User flipped queen");
+var flipCard = function(cardId) {
+		cardsInPlay.push(cards[cardId]);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped king")
 
 			if (cardsInPlay.length === 2) {
-
-			if (cardsInPlay[0] === cardsInPlay[1]) {
-				alert("You found a match!");
-			} else {
-				alert("Sorry, try again.");
+				checkForMatch()
 			}
 
-	}
+			console.log("User flipped " + cards[cardId]);
+			
+		}
 
+flipCard(0);
+flipCard(2);
 
